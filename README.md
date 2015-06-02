0e07243d4cde7b549e40fb2684d38f2bad9b278d
Charles Lai <charles.lai@datadoghq.com>
2015-06-02 16:24:02 -0400

    yeeto mosquito

---
+++ README.md
@@ -0,0 +1,7 @@
+# BANG BANG
+bang bang
+
+# Installation
+Clone this repo and install dependencies via `pip install -r requirementes.txt` inside a virtualenv. Add Twitter app secrets to your environment and run with `python main.py` to start scheduling tweets. 
+
+Or, install the heroku toolbelt, create a `.env` file containing your Twitter secrets, and run the app with `heroku local`. You can then host the app on Heroku as well.
---
+++ main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import pytz
+import datetime
+import os
+from flask import Flask
+from twython import Twython
+
+app = Flask(__name__)
+CONSUMER_KEY=os.environ.get('CONSUMER_KEY')
+CONSUMER_SECRET=os.environ.get('CONSUMER_SECRET')
+ACCESS_TOKEN=os.environ.get('ACCESS_TOKEN')
+ACCESS_TOKEN_SECRET=os.environ.get('ACCESS_TOKEN_SECRET')
+
+@app.route('/')
+def tweet():
+    # Generate the message that the bot will tweet
+    msg = "BANG BANG"
+
+    # Tweet the message out
+    twitter = Twython(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    result = twitter.update_status(status=msg)
+    return result
+
+if __name__ == '__main__':
+    app.run()
