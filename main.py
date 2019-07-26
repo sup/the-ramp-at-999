94474f89c673b7353c857802f4d000c0763c20fe
Charles Lai <charles.lai@airbnb.com>
2019-07-26 14:43:39 -0700

    timezone

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ ACCESS_TOKEN_SECRET=os.environ.get('ACCESS_TOKEN_SECRET')
 @app.route('/')
 def tweet():
     # Generate the message that the bot will tweet
-    msg = "BANG BANG occurred at " + str(datetime.datetime.now())
+    tz = pytz.timezone("US/Pacific")
+    msg = "BANG BANG occurred at " + str(datetime.datetime.now(tz))
 
     # Tweet the message out
     twitter = Twython(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
